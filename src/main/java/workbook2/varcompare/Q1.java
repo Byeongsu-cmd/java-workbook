b8107c686638b394398d827fc408d5be62104f2a
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-17 17:22:15 +0900

    varcompare Q1 구현

--- src/main/java/workbook2/varcompare/Q1.java
+++ src/main/java/workbook2/varcompare/Q1.java
@@ -1,18 +1,24 @@
 package main.java.workbook2.varcompare;
 
 class Counter {
-    static int count = 0;
-    int id;
+    static int count = 0; // 클래스 변수 선언
+    int id; // 인스턴스 변수 선언
+
     Counter() {
         // TODO: count 증가 후 id에 할당
+        count++;
+        id += count;
     }
 }
 
 public class Q1 {
     public static void main(String[] args) {
-        Counter c1 = new Counter();
-        System.out.println(c1.id + " " + Counter.count);
-        Counter c2 = new Counter();
-        System.out.println(c2.id + " " + Counter.count);
+        Counter c1 = new Counter(); // 객체 생성
+
+        System.out.println(c1.id + " " + Counter.count); // 객체를 통해 id,count 값 출력
+
+        Counter c2 = new Counter(); // 객체 생성
+
+        System.out.println(c2.id + " " + Counter.count); // 객체를 통해 id,count 값 출력
     }
 }
