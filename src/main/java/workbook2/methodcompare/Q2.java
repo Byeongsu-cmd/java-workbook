0a63e482c4330946a9740465622a7f23bf9832a9
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-18 19:58:26 +0900

    methodcompare Q2 구현

--- src/main/java/workbook2/methodcompare/Q2.java
+++ src/main/java/workbook2/methodcompare/Q2.java
@@ -1,25 +1,39 @@
 package main.java.workbook2.methodcompare;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class MathUtil {
-    static int square(int n) {
+    static int square(int n) { // 클래스 메서드
         // TODO: n의 제곱 반환
-        return 0;
+        return n * n;
     }
-    int cube(int n) {
+
+    int cube(int n) { // 인스턴스 메서드
         // TODO: n의 세제곱 반환
-        return 0;
+        return n * n * n;
     }
 }
 
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sq = MathUtil.square(n);
-        MathUtil util = new MathUtil();
-        int cu = util.cube(n);
-        // TODO: sq와 cu 출력
+
+        try {
+            int n = sc.nextInt(); // 계산하길 바라는 정수 입력
+
+            int sq = MathUtil.square(n); // 클래스 메서드 호출
+
+            MathUtil util = new MathUtil(); // 객체 생성
+
+            int cu = util.cube(n); // 인스턴스 변수 호출
+
+            // TODO: sq와 cu 출력
+            System.out.println(sq + " " + cu);
+        } catch (InputMismatchException e) { // 숫자 이외의 값을 입력 시
+            System.out.println("숫자만 입력해 주세요!"); // 예외 처리
+        } finally { // 항상 실행
+            sc.close(); // 자원 누수 방지
+        }
     }
 }
