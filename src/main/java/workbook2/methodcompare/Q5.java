cbebfe7bfe5bd3a11bb7fcb81f7bf4a1f75f5bbf
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-27 20:53:57 +0900

    methodcompare Q5 구현

--- src/main/java/workbook2/methodcompare/Q5.java
+++ src/main/java/workbook2/methodcompare/Q5.java
@@ -1,27 +1,42 @@
 package main.java.workbook2.methodcompare;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Statistics {
     static int count = 0;
     static int sum = 0;
+
     void addNumber(int n) {
         // TODO: count와 sum 갱신
+        sum += n;
+        count++;
     }
+
     static double average() {
         // TODO: 평균 반환
-        return 0;
+        if (count == 0) { // 입력이 0일 때 예외 처리
+            return 0;
+        }
+        return (double) sum / count;
     }
 }
 
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Statistics stat = new Statistics();
-        for (int i = 0; i < n; i++) {
-            stat.addNumber(sc.nextInt());
+        try {
+            int n = sc.nextInt();
+            Statistics stat = new Statistics();
+            for (int i = 0; i < n; i++) {
+                stat.addNumber(sc.nextInt());
+            }
+            // TODO: Statistics.average() 출력
+            System.out.println(Statistics.average());
+        } catch (InputMismatchException e) { // 숫자 이외의 값을 입력 시
+            System.out.println("숫자만 입력해 주세요!"); // 예외 처리
+        } finally { // 항상 실행
+            sc.close(); // 자원 누수 방지
         }
-        // TODO: Statistics.average() 출력
     }
 }
